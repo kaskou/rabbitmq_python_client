a14f29254ee01aa8d54b5e6e69195022fcef5d95
kaskou <sangemkoushik@gmail.com>
2021-08-05 15:15:43 +1000

    name of the package fix

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(
-    name='rabbitmq_python_client',
+    name='rabbitmq_api_client',
     version='1.0.0',
     description='Python client for rabbitmq api',
     url='git@github.com:kaskou/rabbitmq_python_client.git',
     author='koukas',
     author_email='sangemkoushik@gmail.com',
     license='MIT LICENSE',
-    packages=['rabbitmq_python_client'],
+    packages=['rabbitmq_api_client'],
     zip_safe=False
 )
